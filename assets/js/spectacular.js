45eece10fd4a190885e2bb30e80ede3903761d10
Omar Badran <32434709+omarbdrn@users.noreply.github.com>
2023-03-07 23:15:38 +0200

    Update spectacular.js

--- assets/js/spectacular.js
+++ assets/js/spectacular.js
@@ -1,3 +1,1 @@
-window.open("https://github.com/omarbdrn","_blank");
-window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");
-document.location = 'https://github.com/omarbdrn';
+document.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
