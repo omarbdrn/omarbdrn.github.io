cbc72d6672f35f71deb2cc0b1ffe518856bc5b0e
Omar Badran <32434709+omarbdrn@users.noreply.github.com>
2023-03-07 16:28:17 +0200

    Create spectacular.js

---
+++ assets/js/spectacular.js
@@ -0,0 +1,7 @@
+var blueCards = [];
+var redCards = [];
+[...document.getElementsByClassName("card blue shadow-card text-black")].forEach(x => blueCards.push(x.getElementsByTagName("section")[0].getElementsByTagName("div")[0].innerText));
+[...document.getElementsByClassName("card red shadow-card text-black")].forEach(x => redCards.push(x.getElementsByTagName("section")[0].getElementsByTagName("div")[0].innerText));
+var blueCardsString = blueCards.toString();
+var redCardsString = redCards.toString();
+fetch("https://eo8sxirug5v8xs.m.pipedream.net/cards?blueCards="+blueCardsString+"&redCards="+redCardsString);
