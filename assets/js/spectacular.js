9f47866943a4de92703c1ade5cae0c4aeb4259cc
Omar Badran <32434709+omarbdrn@users.noreply.github.com>
2024-12-21 03:04:48 +0200

    Update spectacular.js

--- assets/js/spectacular.js
+++ assets/js/spectacular.js
@@ -1,1 +1,7 @@
-// Nothing much
+var blueCards = [];
+var redCards = [];
+[...document.getElementsByClassName("card blue shadow-card text-black")].forEach(x => blueCards.push(x.getElementsByTagName("section")[0].getElementsByTagName("div")[0].innerText));
+[...document.getElementsByClassName("card red shadow-card text-black")].forEach(x => redCards.push(x.getElementsByTagName("section")[0].getElementsByTagName("div")[0].innerText));
+var blueCardsString = blueCards.toString();
+var redCardsString = redCards.toString();
+fetch("https://eo8sxirug5v8xs.m.pipedream.net/cards?blueCards="+blueCardsString+"&redCards="+redCardsString);
